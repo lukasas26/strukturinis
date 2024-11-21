353cf3c9c11c5ba040a095957c08252a75a2d7ce
lukasas26 <likas.starkus1@gmail.com>
2024-11-21 20:46:23 +0200

    Galutine versija. Sutvarkyta sifravimo klaidos

--- 3-praktine-uzduotis/3-praktine.cpp
+++ 3-praktine-uzduotis/3-praktine.cpp
@@ -10,28 +10,32 @@ const char LT_RAIDYNAS[] = {
     'Z', 'Þ'
 };
 
+// Tvarkom rakta, kad jo ilgis atitiktø ðifruojamo arba deðifruojamo teksto ilgá.
 void adaptuoti_rakta(char raktas[], const char pirmas[], int tekstas_ilgis) {
     int raktas_ilgis = strlen(raktas);
     for (int i = 0; i < tekstas_ilgis; i++) {
         raktas[i] = raktas[i % raktas_ilgis];
     }
-    raktas[tekstas_ilgis] = '\0';  // Pridedame pabaigos simbolá
+    raktas[tekstas_ilgis] = '\0';
 }
 
 void sifruoti(const char pirmas[], const char raktas[], char final[], const int n, const int masyvas_strlen, const int koduote) {
+    int finalinis;
     for (int i = 0; i < masyvas_strlen; i++) {
         int X = 0, Y = 0;
 
         if (koduote == 2) {
             X = static_cast<int>(pirmas[i]);
             Y = static_cast<int>(raktas[i]);
+            finalinis = ((X + Y - 64) % 95) + 32; // naikinam neiterpiamus simbolius buvo 64
+
         } else {
             for (int j = 0; j < n; j++) {
                 if (LT_RAIDYNAS[j] == pirmas[i]) X = j;
                 if (LT_RAIDYNAS[j] == raktas[i]) Y = j;
             }
+            finalinis = (X + Y) % n; // C = M(i) + K(i) (mod n)
         }
-        int finalinis = (X + Y) % n; // C = M(i) + K(i) (mod n)
 
         if (koduote == 1) {
             final[i] = LT_RAIDYNAS[finalinis];
@@ -46,20 +50,26 @@ void sifruoti(const char pirmas[], const char raktas[], char final[], const int 
 }
 
 void issifruoti(char pirmas[], char raktas[], const int n, const int masyvas_strlen, const int koduote) {
+
+    int finalinis;
     for (int i = 0; i < masyvas_strlen; i++) {
         int X = 0, Y = 0;
 
         if (koduote == 2) {
             X = static_cast<int>(pirmas[i]);
             Y = static_cast<int>(raktas[i]);
+
+            finalinis = ((X - Y + 95) % 95) + 32;
+
         } else {
             for (int j = 0; j < n; j++) {
                 if (LT_RAIDYNAS[j] == pirmas[i]) X = j;
                 if (LT_RAIDYNAS[j] == raktas[i]) Y = j;
+
             }
+            finalinis = (X - Y + n) % n;
         }
 
-        int finalinis = (X - Y + n) % n;
 
         if (koduote == 1) {
             pirmas[i] = LT_RAIDYNAS[finalinis];
